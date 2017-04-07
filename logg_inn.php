3dce2004e53b10ffde0c1d5673c686e1fa6558ac
markusvhagen <markusvhagen@gmail.com>
2017-04-07 18:44:16 +0200

    Check if POST-details matches with DB.

--- logg_inn.php
+++ logg_inn.php
@@ -1,23 +1,48 @@
 <?php
 
+session_start();
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "auth-system";
+$kontakt = mysql_connect($servername, $username, $password);
 
-$brukernavn = $_POST["l_brukernavn"];
-$passord = $_POST["l_passord"];
-
-mysql_connect($servername, $username, $password);
-mysql_select_db($dbname);
-
-if (mysql_connect($servername, $username, $password) === false) {
-    echo "Tilkobling mislyktes: " . $conn->connect_error;
+// Hvis det ikke er mulig å opprette en databasetilkobling.
+if (!$kontakt) {
+  echo "Mislyktes med databasetilkoblingen.";
 }
 
-mysql_close();
+else {
+  mysql_select_db($dbname, $kontakt);
 
- ?>
+  $brukernavn = $_POST["l_brukernavn"];
+  $passord = $_POST["l_passord"];
+  // Spørringen under henter ut passordet for et gitt brukernavn.
+  $sql = "SELECT passord FROM `auth-system-brukere` WHERE brukernavn='$brukernavn'";
+
+  $forespoersel = mysql_query($sql);
+
+  // Hvis det faktisk eksisterer en forespørsel med spørringen over.
+  if ($forespoersel) {
+      // Disse to linjene gjør om resultatet fra spørringen til en lesbar streng.
+      $resultat = mysql_fetch_assoc($forespoersel);
+      $sql_passord = $resultat['passord'];
+  }
+
+  if ($sql_passord == $passord) {
+      echo "Du er nå logget inn!";
+  }
+
+  else {
+      $_SESSION['logg_inn_sjekk'] = false;
+      header("location: index.php");
+  }
+
+  mysql_close();
+}
+
+?>
 
 <!DOCTYPE html>
 <html>

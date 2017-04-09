297d759359a9083e988d42255d35e84cd216bac0
markusvhagen <markusvhagen@gmail.com>
2017-04-09 22:46:49 +0200

    Check for availability of username with AJAX

---
+++ src/js/tilgjengeligBrukernavn.js
@@ -0,0 +1,29 @@
+var output_tilgjengelighet = document.getElementById("brukernavnTilgjengelighet");
+
+function tilgjengeligBrukernavn(str) {
+
+  if (str == 0) {
+    output_tilgjengelighet.innerHTML = "";
+  }
+
+  else {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      output_tilgjengelighet.innerHTML = this.responseText;
+
+      switch (this.responseText) {
+
+        case "tilgjengelig":
+          output_tilgjengelighet.style.color = "green";
+          break;
+        case "opptatt":
+          output_tilgjengelighet.style.color = "red";
+          break;
+      }
+
+    };
+    xmlhttp.open("GET", "../src/php/tilgjengeligBrukernavn.php?q="+str, true);
+    xmlhttp.send();
+  }
+
+}
---
+++ src/php/tilgjengeligBrukernavn.php
@@ -0,0 +1,24 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "auth-system";
+$tilkobling = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$password");
+$brukernavn = $_REQUEST['q'];
+$brukernavnEksistererQuery = "SELECT brukernavn FROM `auth-system-brukere` WHERE brukernavn = ?";
+$brukernavnEksisterer = $tilkobling->prepare($brukernavnEksistererQuery);
+$brukernavnEksisterer->execute(array($brukernavn));
+$eksistens = $brukernavnEksisterer->fetchAll(PDO::FETCH_ASSOC);
+
+if (empty($eksistens)) {
+  echo "tilgjengelig";
+}
+
+else {
+  echo "opptatt";
+}
+
+$tilkobling = null;
+
+?>
